fb388ef5757f6dca9bb465d779cbc4ab0a0dd10e
Stolz <Stolz@users.noreply.github.com>
2015-02-22 14:40:55 +0100

    Middleware decoupled from parser

--- src/Middleware.php
+++ src/Middleware.php
@@ -1,6 +1,10 @@
 <?php namespace Stolz\HtmlTidy;
 
-class Middleware implements \Illuminate\Contracts\Routing\Middleware
+use Illuminate\Contracts\Routing\Middleware as BaseMiddleware;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class Middleware implements BaseMiddleware
 {
 	/**
 	 * Handle an incoming request.
@@ -11,6 +15,33 @@ class Middleware implements \Illuminate\Contracts\Routing\Middleware
 	 */
 	public function handle($request, \Closure $next)
 	{
-		return app('stolz.tidy')->handle($request, $next($request));
+		$response = $next($request);
+
+		// Check PHP extension
+		if( ! extension_loaded('tidy') or ! config('tidy.enabled'))
+			return $response;
+
+		// Check request
+		if($request->ajax() and ! config('tidy.ajax'))
+			return $response;
+
+		// Skip redirects
+		if($response instanceof RedirectResponse)
+			return $response;
+
+		// Convert unknown responses
+		if( ! $response instanceof Response)
+		{
+			$response = new Response($response);
+			if( ! $response->headers->has('content-type'))
+				$response->headers->set('content-type', 'text/html');
+		}
+
+		// If response is HTML parse it
+		$contentType = $response->headers->get('content-type');
+		if(str_contains($contentType, 'text/html'))
+			$response->setContent(app('stolz.tidy')->parse($response->getContent()));
+
+		return $response;
 	}
 }
